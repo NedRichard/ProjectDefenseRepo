36b7037eaa11aae370eccfb06a67da70280be2ba
Néd <nedimerichard@gmail.com>
2023-07-17 21:39:04 +0300

    UnitSelectedVisual

--- ProjectDefense/Assets/Scripts/UnitActionSystem.cs
+++ ProjectDefense/Assets/Scripts/UnitActionSystem.cs
@@ -1,18 +1,36 @@
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
+    public static UnitActionSystem Instance { get; private set; }
+
+    public event EventHandler OnSelectedUnitChanged;
+
     [SerializeField] private Unit selectedUnit;
-    [SerializeField] public LayerMask UnitLayerMask;
+    [SerializeField] private LayerMask UnitLayerMask;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There's more than one UnitActionSystem!" + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+            Instance = this;
+    }
 
     private void Update()
     {
-        if (TryHandleUnitSelection()) return;
         
         if (Input.GetMouseButtonDown(0))
         {
+            if (TryHandleUnitSelection()) return;
+
             selectedUnit.Move(MouseWorld.GetPosition());
         }
     }
@@ -24,11 +42,26 @@ public class UnitActionSystem : MonoBehaviour
         {
             if(raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
                 return true;
             }
         }
 
         return false;
+    }
+
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+
+        //Trigger Event only if OnSelectedUnitChanged isn't null
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+
+        
+    }
+
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
     }
 }
