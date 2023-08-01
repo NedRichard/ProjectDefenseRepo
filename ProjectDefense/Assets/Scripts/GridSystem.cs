e12e9ff00be6923fbd7f4f1e37abd241a790701f
Néd <nedimerichard@gmail.com>
2023-08-01 22:04:29 +0300

    Grid System + start Grid Object

--- ProjectDefense/Assets/Scripts/GridSystem.cs
+++ ProjectDefense/Assets/Scripts/GridSystem.cs
@@ -7,18 +7,23 @@ public class GridSystem
     private int width;
     private int height;
     private float cellSize;
+    private GridObject[,] gridObjectArray;
     public GridSystem(int width, int height, float cellSize)
     {
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
 
+        gridObjectArray = new GridObject[width, height];
+
         //Cycle through the grid
         for (int x = 0; x < width; x++)
         {
             for (int z = 0; z < height; z++)
             {
-                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z) + Vector3.right * 2f, Color.white, 1000);
+                //Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z) + Vector3.right * 2f, Color.white, 1000);
+                GridPosition gridPosition = new GridPosition(x, z);
+                gridObjectArray[x, z] = new GridObject(this, gridPosition);
             }
         }
     }
@@ -26,6 +31,14 @@ public class GridSystem
         {
             return new Vector3(x, 0, z) * cellSize;
         }
+
+    public GridPosition GetGridPosition(Vector3 worldPosition)
+    {
+        return new GridPosition(
+            Mathf.RoundToInt(worldPosition.x / cellSize),
+            Mathf.RoundToInt(worldPosition.z / cellSize)
+            );
+    }
    
 
 }
--- ProjectDefense/Assets/Scripts/Testing.cs
+++ ProjectDefense/Assets/Scripts/Testing.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    private GridSystem gridSystem;
     private void Start()
     {
-        new GridSystem(10, 10);
+        gridSystem = new GridSystem(10, 10, 2f);
+
+        Debug.Log(new GridPosition(5,7));
     }
 
-    
+    private void Update()
+    {
+        Debug.Log(gridSystem.GetGridPosition(MouseWorld.GetPosition()));
+    }
+
 }
