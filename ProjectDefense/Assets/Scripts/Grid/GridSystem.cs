738bb7e15cb005c80f52a214a541a65eafda061f
Néd <nedimerichard@gmail.com>
2023-08-02 14:58:52 +0300

    Grid Object + Grid Object Debug

--- ProjectDefense/Assets/Scripts/Grid/GridSystem.cs
+++ ProjectDefense/Assets/Scripts/Grid/GridSystem.cs
@@ -8,11 +8,13 @@ public class GridSystem
     private int height;
     private float cellSize;
     private GridObject[,] gridObjectArray;
+
     public GridSystem(int width, int height, float cellSize)
     {
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
+
 
         gridObjectArray = new GridObject[width, height];
 
@@ -27,9 +29,9 @@ public class GridSystem
             }
         }
     }
-        public Vector3 GetWorldPosition(int x, int z)
+        public Vector3 GetWorldPosition(GridPosition gridPosition)
         {
-            return new Vector3(x, 0, z) * cellSize;
+            return new Vector3(gridPosition.x, 0, gridPosition.z) * cellSize;
         }
 
     public GridPosition GetGridPosition(Vector3 worldPosition)
@@ -39,6 +41,26 @@ public class GridSystem
             Mathf.RoundToInt(worldPosition.z / cellSize)
             );
     }
+
+    public void CreateDebugObjects(Transform debugPrefab)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                GridPosition gridPosition = new GridPosition(x, z);
+                
+               Transform debugTransform = GameObject.Instantiate(debugPrefab, GetWorldPosition(gridPosition), Quaternion.identity);
+               GridDebugObject gridDebugObject = debugTransform.GetComponent<GridDebugObject>();
+               gridDebugObject.SetGridObject(GetGridObject(gridPosition));
+               
+            }
+        }
+    }
    
+    public GridObject GetGridObject(GridPosition gridPosition)
+    {
+        return gridObjectArray[gridPosition.x, gridPosition.z];
+    }
 
 }
