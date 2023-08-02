d3a5ea6c36869c82020aa5e0d3e8a7b395fcf0c7
Néd <nedimerichard@gmail.com>
2023-08-02 15:20:43 +0300

    GridDebug changes

--- ProjectDefense/Assets/Scripts/Grid/GridDebugObject.cs
+++ ProjectDefense/Assets/Scripts/Grid/GridDebugObject.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class GridDebugObject : MonoBehaviour
 {
     [SerializeField] private TextMeshPro textMeshPro;
+
     private GridObject gridObject;
 
     public void SetGridObject(GridObject gridObject)
